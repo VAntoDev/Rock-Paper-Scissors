fd440e39460cfebc50b3817578007b5c41199dca
VAnto <vntantonio@gmail.com>
2022-09-15 12:34:17 +0200

    Add playRound()

--- myscript.js
+++ myscript.js
@@ -1,17 +1,42 @@
-console.log("Hi!")
-
-//Crea una funzione che da il risultato random del computer: ti servono i risultati che il computer può avere, una randomizzatore e una variabile in 
-//cui mettere il risultato
-
-let computerChoice = "";
+let computerSelection;
 const choices = [ "ROCK" , "PAPER", "SCISSORS"]
 
-function getComputerChoice(){
-    computerChoice = choices[Math.floor(Math.random()*choices.length)];
-    alert(computerChoice)
-    return computerChoice;
+//getComputerSelection gets the cpu's choice for the game
+function getComputerSelection(){
+    computerSelection = choices[Math.floor(Math.random()*choices.length)];
+    return computerSelection;
 }
 
+function capitalize(string){
+    return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
+}
 
+//playRound plays a round of the game
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+    switch (true) {       
+        //Tie
+        case playerSelection === computerSelection:
+            return result = `Its a tie! ${playerSelection} and ${computerSelection} are the same`;
+        //Lose
+        case playerSelection === "Rock" && computerSelection === "Paper":
+            return result = `You lose, ${playerSelection} beats ${computerSelection}`;
+        case playerSelection === "Paper" && computerSelection === "Scissors":
+            return result = `You lose, ${playerSelection} beats ${computerSelection}`;
+        case playerSelection === "Scissors" && computerSelection === "Rock":
+            return result = `You lose, ${playerSelection} beats ${computerSelection}`;
+        //Win
+        case playerSelection === "Rock" && computerSelection === "Scissors":
+            return result = `You win! ${playerSelection} beats ${computerSelection}`;
+        case playerSelection === "Paper" && computerSelection === "Rock":
+            return result = `You win! ${playerSelection} beats ${computerSelection}`;
+        case playerSelection === "Scissors" && computerSelection === "Paper":
+            return result = `You win! ${playerSelection} beats ${computerSelection}`;
 
-console.log(getComputerChoice())
+    }
+  }
+
+  let playerSelection = prompt("Lets play Rock Paper Scissors! Choose one of the three")
+  computerSelection = getComputerSelection();
+  alert(playRound(playerSelection, computerSelection));
