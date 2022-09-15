0302ff7cdea1cc2f154352c8e3336e6ad32bae62
VAnto <vntantonio@gmail.com>
2022-09-15 17:03:28 +0200

    Add game()

--- myscript.js
+++ myscript.js
@@ -1,4 +1,10 @@
 let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let prevPlayerScore = 0;
+let prevComputerScore = 0;
+let win = true;
 const choices = [ "ROCK" , "PAPER", "SCISSORS"]
 
 //getComputerSelection gets the cpu's choice for the game
@@ -21,22 +27,62 @@ function playRound(playerSelection, computerSelection) {
             return result = `Its a tie! ${playerSelection} and ${computerSelection} are the same`;
         //Lose
         case playerSelection === "Rock" && computerSelection === "Paper":
+            computerScore++;
             return result = `You lose, ${playerSelection} beats ${computerSelection}`;
         case playerSelection === "Paper" && computerSelection === "Scissors":
+            computerScore++;
             return result = `You lose, ${playerSelection} beats ${computerSelection}`;
         case playerSelection === "Scissors" && computerSelection === "Rock":
+            computerScore++;
             return result = `You lose, ${playerSelection} beats ${computerSelection}`;
         //Win
         case playerSelection === "Rock" && computerSelection === "Scissors":
+            playerScore++;
             return result = `You win! ${playerSelection} beats ${computerSelection}`;
         case playerSelection === "Paper" && computerSelection === "Rock":
+            playerScore++;
             return result = `You win! ${playerSelection} beats ${computerSelection}`;
         case playerSelection === "Scissors" && computerSelection === "Paper":
+            playerScore++;
             return result = `You win! ${playerSelection} beats ${computerSelection}`;
-
+        //Not valid input
+        default:
+            return result = "That's not a valid choice, retry";
+            
     }
   }
 
-  let playerSelection = prompt("Lets play Rock Paper Scissors! Choose one of the three")
-  computerSelection = getComputerSelection();
-  alert(playRound(playerSelection, computerSelection));
+  //game() lets the game repeat up to 5 times
+  function game(){
+
+        //this while is necessary for the game to run smoothly even if there's a tie
+        while(win){
+
+            playerSelection = prompt("Lets play Rock Paper Scissors! Choose one of the three")
+            computerSelection = getComputerSelection();
+            alert(playRound(playerSelection, computerSelection));
+
+            if (computerScore != prevComputerScore || playerScore != prevPlayerScore){
+                prevComputerScore = computerScore;
+                prevPlayerScore = playerScore;
+                
+            } 
+            
+            alert(`Your score is ${playerScore} and your opponent's score is ${computerScore}`)
+
+            if (computerScore === 5) {
+                alert(`Cpu won! ${playerScore} - ${computerScore} `)
+                win = false;
+            } else if (playerScore === 5){
+                alert(`You won! ${playerScore} - ${computerScore} `)
+                win = false;
+            }
+
+            
+        }
+
+        
+        
+      }
+
+game();
