06fbb2fec04298ce3df27789c603c9622d3c04aa
VAnto <vntantonio@gmail.com>
2022-10-13 01:09:48 +0200

    Add css-and-images ui

--- myscript.js
+++ myscript.js
@@ -19,7 +19,7 @@ function capitalize(string){
 
 //playRound plays a round of the game
 function playRound(playerSelection, computerSelection) {
-    playerSelection = capitalize(playerSelection);
+    //playerSelection = capitalize(playerSelection);
     computerSelection = capitalize(computerSelection);
     switch (true) {       
         //Tie
@@ -52,15 +52,20 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
-  //game() lets the game repeat up to 5 times
+  function gameRestart(){
+      playerScore = 0;
+      prevPlayerScore = 0
+      computerScore = 0;
+      prevComputerScore = 0;
+  }
+
+  //game() lets the game repeat up to n times
   function game(){
+        
 
-        //this while is necessary for the game to run smoothly even if there's a tie
-        while(win){
-
-            playerSelection = prompt("Lets play Rock Paper Scissors! Choose one of the three")
+            //playerSelection = prompt("Lets play Rock Paper Scissors! Choose one of the three")
             computerSelection = getComputerSelection();
-            alert(playRound(playerSelection, computerSelection));
+            gameExecution.textContent =(playRound(playerSelection, computerSelection));
 
             if (computerScore != prevComputerScore || playerScore != prevPlayerScore){
                 prevComputerScore = computerScore;
@@ -68,21 +73,72 @@ function playRound(playerSelection, computerSelection) {
                 
             } 
             
-            alert(`Your score is ${playerScore} and your opponent's score is ${computerScore}`)
+            //This part of the code makes the animations work
+            resultsContainer.removeChild(resultsP);
+            resultsContainer.insertBefore(resultsP, buttonContainer);
+            resultsContainer.removeChild(gameExecution);
+            resultsContainer.insertBefore(gameExecution, gameRestartP);
+            resultsContainer.removeChild(gameRestartP);
+            resultsContainer.appendChild(gameRestartP)
 
-            if (computerScore === 5) {
-                alert(`Cpu won! ${playerScore} - ${computerScore} `)
-                win = false;
-            } else if (playerScore === 5){
-                alert(`You won! ${playerScore} - ${computerScore} `)
-                win = false;
-            }
-
+            resultsP.textContent = (`Your score is ${playerScore} and your opponent's score is ${computerScore}`);
             
-        }
 
-        
+            if (computerScore === 3) {
+                resultsP.textContent =(`Cpu won! ${playerScore} - ${computerScore}`);
+                gameRestartP.textContent =("Restarting the game, both scores down to 0");
+                gameRestart()
+            } else if (playerScore === 3){
+                resultsP.textContent =(`You won! ${playerScore} - ${computerScore}`);
+                gameRestartP.textContent =("Restarting the game, both scores down to 0");
+                gameRestart()
+            }
         
       }
 
-game();
+function stopButtonClick() {
+    rockSelection.disabled = true;
+    scissorsSelection.disabled = true;
+    paperSelection.disabled = true;
+    setTimeout(()=>{
+    rockSelection.disabled = false;
+    scissorsSelection.disabled = false;
+    paperSelection.disabled = false;
+    console.log('Button Activated')}, 500)
+}
+
+const rockSelection = document.querySelector('#rock')
+const paperSelection = document.querySelector('#paper')
+const scissorsSelection = document.querySelector('#scissors')
+const resultsP = document.querySelector('#results')
+const gameRestartP = document.querySelector('#game-restart')
+const gameExecution = document.querySelector('#game-execution')
+const resultsContainer = document.querySelector('#results-container')
+const buttonContainer = document.querySelector('#button-container')
+
+resultsP.textContent = "Lets play Rock Paper Scissors! Choose one of them to start."
+
+rockSelection.addEventListener('click', function (e){
+    gameRestartP.textContent =("")
+    playerSelection = "Rock";
+    game();
+    stopButtonClick();
+    
+})
+
+paperSelection.addEventListener('click', function (e){
+    gameRestartP.textContent =("")
+    playerSelection = "Paper";
+    game();
+    stopButtonClick();
+})
+
+scissorsSelection.addEventListener('click', function (e){
+    gameRestartP.textContent =("")
+    playerSelection = "Scissors";
+    game();
+    stopButtonClick();
+})
+
+
+//game();
